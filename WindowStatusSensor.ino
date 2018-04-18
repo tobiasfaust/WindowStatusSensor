4173ed3270958eea998d9608af14f2f1c8a7509f
tobiasfaust <tobias.faust@gmx.net>
2018-04-18 20:12:27 +0200

    Initial commit

---
+++ WindowStatusSensor.ino
@@ -0,0 +1,379 @@
+/*
+ * bininps2
+ *
+ * Copyright (c) 2015 panStamp <contact@panstamp.com>
+ * 
+ * This file is part of the panStamp project.
+ * 
+ * panStamp  is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * any later version.
+ * 
+ * panStamp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with panStamp; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ * 
+ * Author: Daniel Berenguer
+ * Creation date: 04/01/2015
+ *
+ * Device:
+ * Binary input + counter module
+ *
+ * Description:
+ * Device that reports the binary state of 12 digital inputs, 4 of them being
+ * used as pulse counters as well.
+ *
+ * PANSTAMP AVR
+ * This sketch is not supported by panStamp AVR.
+ * panStamp AVR does not support attachInterrupt on pins other than INT1 (D3).
+ * RTC calendar mode is not supported by AVR modules either.
+ * With panStamp AVR use the classic "bininps" sketch or "pulsecounter"
+ *
+ * PANSTAMP NRG
+ * Binary inputs only!
+ * This sketch uses internal pull-up's. Counters increment when inputs turn down
+ * (LOW state)
+ *
+ * This sketch can be used to detect key/switch presses, binary alarms or
+ * any other binary sensor. It can also be used to count pulses from a vaste
+ * variety of devices such as energy meters, water meters, gas meters, etc.
+ * You may want to add some delays in updateValues in order to better debounce
+ * the binary states. We suggest to add external capacitors to the inputs as well.
+ * Capacitor values and delays should depend on the nature and frequence of the
+ * input signals.
+ *
+ * This device is NOT low-power enabled so it will not enter low-power mode just
+ * after reading the binary states and transmitting them over the SWAP
+ * network. Powering over battery is not recommend
+ *
+ * Associated Device Definition File, defining registers, endpoints and
+ * configuration parameters:
+ * windowstatussensor.xml (Window Status Sensor)
+ */
+ 
+#include "swap.h"
+#include "regtable.h"
+#include "HardwareSerial.h"
+
+/* #define DEBUG */
+
+#define cnt0 10
+#define cnt1 11
+#define cnt2 12
+#define cnt3 13
+#define cnt4 14
+#define cnt5 15
+#define cnt6 9
+#define cnt7 8
+
+#define LED_R 22
+#define LED_Y 21
+#define LED_G 20
+
+/**
+ * Maximum repeating count
+ */
+const byte maxRepeaterHop = 1;
+
+/**
+ * Register data containers made global
+ */
+byte dtBinInputs[1];    // Binary input states
+boolean raisedIRQ = false;
+static unsigned long last_transmission_time = 0;
+uint8_t group_assignment[8] = {0,0,0,0,0,0,0,0}; // welcher Pin ist welchem Port zugeordnet? -> group_assignment[PinNumber] = GroupNumber
+                                                 // Gruppe 0 - 7 ist valid
+uint8_t group_count[8]      = {0,0,0,0,0,0,0,0}; // wieviele Pins sind welcher Gruppe zugeordnet? -> group_count[GroupNumber] = <anzahl>
+uint8_t group_state[8]      = {0,0,0,0,0,0,0,0}; // Status der einzelnen Gruppe -> group_state[GroupNumber] = Status
+                                                 // Status: 0 => closed
+                                                 // Status: 1 => tilted
+                                                 // Status: 2 => opened
+                                                 
+unsigned int LED_state = 0;               // Overall Status; 0 => closed, 1 => tilted, 2 => opened
+unsigned int LED_R_State = LOW;           // ledState used to set the Red LED
+unsigned int LED_Y_State = LOW;           // ledState used to set the Yellow LED
+unsigned int LED_G_State = LOW;           // ledState used to set the Green LED
+unsigned long LED_BlinkOnTime = 0;        // interval for blinking LED, set in loop, eg. 50ms
+unsigned long LED_PrevMillis = 0;         // will store last time LED was updated
+boolean send_config = false;              // if true, send config in loop
+//variable initiaization, setup by configuration 
+unsigned long LED_BlinkOnEvtTime = 5000;  // Blink for ms if an IRQ occurs  
+unsigned long LED_BlinkOnIdleTime = 50;   // Blink for ms if an nothing occurs  
+unsigned long LED_BlinkOffTime =  10000;  // during blinking, eg. 10s (50ms on, 10s off)
+
+
+// RTC data
+//RTCDATA rtcData;
+
+/**
+ * check if TXIntervall is reached
+ */
+bool isTimeToTX(){
+  if( millis() - last_transmission_time >= (unsigned long)1000 * swap.txInterval )
+    {   
+      last_transmission_time = millis();
+      return true;
+    }   
+   return false;
+}
+
+/**
+ * handling debug messages
+ */
+void debug(char* txt, bool newline) {
+  #ifdef DEBUG
+  if (newline) {Serial.println(txt);} else {Serial.print(txt);}
+  #endif
+}
+void debug_bin(byte txt, bool newline) {
+  #ifdef DEBUG
+  if (newline) {Serial.println(txt, BIN);} else {Serial.print(txt, BIN);}
+  #endif
+}
+void debug_hex(byte txt, bool newline) {
+  #ifdef DEBUG
+  if (newline) {Serial.println(txt, HEX);} else {Serial.print(txt, HEX);}
+  #endif
+}
+void debug_dec(byte txt, bool newline) {
+  #ifdef DEBUG
+  if (newline) {Serial.println(txt, DEC);} else {Serial.print(txt, DEC);}
+  #endif
+}
+
+/**
+ * Set LED State
+ * LED_state: 0 => closed, 1 => tilted, 2 => opened
+ */
+void SetLEDState(unsigned int state, bool OnOff){
+  // Startzustand herstellen
+  LED_R_State = LOW;
+  LED_Y_State = LOW;
+  LED_G_State = LOW;
+  
+  if (state == 0) LED_G_State = OnOff;
+  else if (state == 1) LED_Y_State = OnOff;
+  else if (state == 2) LED_R_State = OnOff;
+
+  digitalWrite(LED_R, LED_R_State);
+  digitalWrite(LED_Y, LED_Y_State);
+  digitalWrite(LED_G, LED_G_State);
+}
+
+/**
+ * Input Change Interrupts
+ * 1. Bit auf 0 setzen
+ * 2. Bit auf den gemessenen Wert setzen
+ */
+void irq0(void){ dtBinInputs[0] = dtBinInputs[0] & 0xFE; dtBinInputs[0] |= digitalRead(cnt0);      raisedIRQ=true; debug("PinChange 1", true);}
+void irq1(void){ dtBinInputs[0] = dtBinInputs[0] & 0xFD; dtBinInputs[0] |= digitalRead(cnt1) << 1; raisedIRQ=true; debug("PinChange 2", true);}
+void irq2(void){ dtBinInputs[0] = dtBinInputs[0] & 0xFB; dtBinInputs[0] |= digitalRead(cnt2) << 2; raisedIRQ=true; debug("PinChange 3", true);}
+void irq3(void){ dtBinInputs[0] = dtBinInputs[0] & 0xF7; dtBinInputs[0] |= digitalRead(cnt3) << 3; raisedIRQ=true; debug("PinChange 4", true);}
+void irq4(void){ dtBinInputs[0] = dtBinInputs[0] & 0xEF; dtBinInputs[0] |= digitalRead(cnt4) << 4; raisedIRQ=true; debug("PinChange 5", true);}
+void irq5(void){ dtBinInputs[0] = dtBinInputs[0] & 0xDF; dtBinInputs[0] |= digitalRead(cnt5) << 5; raisedIRQ=true; debug("PinChange 6", true);}
+void irq6(void){ dtBinInputs[0] = dtBinInputs[0] & 0xBF; dtBinInputs[0] |= digitalRead(cnt6) << 6; raisedIRQ=true; debug("PinChange 7", true);}
+void irq7(void){ dtBinInputs[0] = dtBinInputs[0] & 0x7F; dtBinInputs[0] |= digitalRead(cnt7) << 7; raisedIRQ=true; debug("PinChange 8", true);}
+
+/**
+ * configPorts
+ *
+ * Configure binary ports to generate interrupts
+ */
+void configPorts(void)
+{
+  // Configure digital pins as inputs with internal pull-up's
+  pinMode(cnt0, INPUT_PULLUP);
+  pinMode(cnt1, INPUT_PULLUP);
+  pinMode(cnt2, INPUT_PULLUP);
+  pinMode(cnt3, INPUT_PULLUP);
+  pinMode(cnt4, INPUT_PULLUP);
+  pinMode(cnt5, INPUT_PULLUP);
+  pinMode(cnt6, INPUT_PULLUP);
+  pinMode(cnt7, INPUT_PULLUP);
+
+  // workaround für Pin 16/17 bei PCB Change 1 (fehlerhaftes Design)
+  pinMode(16, INPUT_PULLUP);
+  pinMode(17, INPUT_PULLUP);
+
+
+  pinMode(LED_R, OUTPUT);
+  pinMode(LED_Y, OUTPUT);
+  pinMode(LED_G, OUTPUT);
+  
+  // Attach pins to custom IRQ's
+  attachInterrupt(cnt0, irq0, CHANGE);
+  attachInterrupt(cnt1, irq1, CHANGE);
+  attachInterrupt(cnt2, irq2, CHANGE);
+  attachInterrupt(cnt3, irq3, CHANGE);
+  attachInterrupt(cnt4, irq4, CHANGE);
+  attachInterrupt(cnt5, irq5, CHANGE);
+  attachInterrupt(cnt6, irq6, CHANGE);
+  attachInterrupt(cnt7, irq7, CHANGE);
+}
+
+/**
+ * updateValues
+ *
+ * Update binary state registers and counters for the first time
+*/ 
+void updateValues(void)
+{
+  irq0();
+  irq1();
+  irq2();
+  irq3();
+  irq4();
+  irq5();
+  irq6();
+  irq7();
+}
+
+/**
+ * setup
+ *
+ * Arduino setup function
+ */
+void setup()
+{
+  panstamp.setHighTxPower();
+  
+  #ifdef DEBUG
+  Serial.begin(38400);
+  #endif
+  
+  debug("Start",true);
+  // neue Parameter in die Variablen laden
+  getVarsFromEeprom();
+    
+  // Configure ports
+  configPorts();
+  digitalWrite(LED_R, LOW);
+  digitalWrite(LED_Y, LOW);
+  digitalWrite(LED_G, LOW);
+  
+  // Init SWAP stack
+  swap.init();
+
+  // Enable repeater mode
+  //swap.enableRepeater(maxRepeaterHop);
+  
+  // Transmit product code
+  swap.getRegister(REGI_PRODUCTCODE)->getData();
+  delay(100);
+  // Enter SYNC state
+  // not Low powered!!
+  swap.enterSystemState(SYSTATE_RXON);
+
+  // During 3 seconds, listen the network for possible commands whilst the LED blinks
+  for(unsigned int i=0 ; i<3 ; i++)
+  {
+    digitalWrite(LED_R, HIGH); delay(100);
+    digitalWrite(LED_R, LOW);  delay(100);
+    digitalWrite(LED_Y, HIGH); delay(100);
+    digitalWrite(LED_Y, LOW);  delay(100);
+    digitalWrite(LED_G, HIGH); delay(100);
+    digitalWrite(LED_G, LOW);  
+    delay(500);
+  }
+  // Transmit periodic Tx interval
+  swap.getRegister(REGI_TXINTERVAL)->getData();
+  delay(100);
+  // Transmit Configuration
+  swap.getRegister(REGI_CONFIGURATION)->getData();
+  delay(100);
+  
+  // update the values initially into the register
+  updateValues();
+  
+  // Transmit initial binary states
+  swap.getRegister(REGI_BININPUTS)->getData();
+  delay(100);
+  
+  // Set current time : Thursday 8-Apr 2015 12:15:00 (PM)
+  //rtcData.year = 2015;
+  //rtcData.mon = 4;
+  //rtcData.day = 9;
+  //rtcData.wday = 4;
+  //rtcData.hour = 13;
+  //rtcData.min = 48;
+  //rtcData.sec = 0;
+  
+  //panstamp.rtc.startCalendar(&rtcData);
+  //panstamp.rtc.setAlarmHour(15);
+  //panstamp.rtc.setAlarmMinutes(50);
+
+  raisedIRQ=false;
+}
+
+/**
+ * loop
+ *
+ * Arduino main loop
+ */
+void loop()
+{
+  unsigned long currentMillis = millis();
+  
+  // Sleep until next (RTC) calendar alarm or pin interrupt
+  //panstamp.sleep();
+  
+  // transmit only if IRQ has raised or TXInterval has reached
+  if (raisedIRQ || isTimeToTX()) {
+    debug("-------------------------------", true);
+    debug("Status: ", false);
+    debug_bin(dtBinInputs[0], true);  
+
+    if (raisedIRQ) {
+      LED_state = 0; // Startzustand
+      // Zustand den LEDs zuweisen
+      for(unsigned int i=1;i<=8;i++) {
+        // gruppe 0 ist default und nicht besetzt
+        // feststellen, welcher Status per LED angezeigt werden soll
+        LED_state = max(group_state[i], LED_state);
+      }
+      
+      SetLEDState(LED_state, HIGH);
+      LED_BlinkOnTime=LED_BlinkOnEvtTime;
+      LED_PrevMillis = currentMillis;
+    }
+
+    // Transmit binary states if an IRQ has raised
+    raisedIRQ=false;
+    swap.getRegister(REGI_BININPUTS)->getData();
+    last_transmission_time = currentMillis;
+
+    delay(100);
+    swap.getRegister(REGI_GROUPSTATES)->getData();
+
+    
+  }  
+
+  // LED blinking
+  // https://www.arduino.cc/en/Tutorial/BlinkWithoutDelay
+  if ((LED_R_State || LED_Y_State || LED_G_State ) && (currentMillis - LED_PrevMillis > LED_BlinkOnTime)) {
+    // eine LED ist an, BlinkOnTime ist abgelaufen
+    SetLEDState(LED_state, LOW);
+    LED_PrevMillis = currentMillis;
+  } else if ((!LED_R_State && !LED_Y_State && !LED_G_State ) && (currentMillis - LED_PrevMillis > LED_BlinkOffTime)) {
+    // alle LEDs sind aus und BlinkOffTime ist abgelaufen
+    SetLEDState(LED_state, HIGH);
+    LED_BlinkOnTime=LED_BlinkOnIdleTime;
+    LED_PrevMillis = currentMillis;
+  }
+
+  if(send_config) {
+    //set by setter proc of configuration (SetConfiguration())
+    delay(100);
+    swap.getRegister(REGI_CONFIGURATION)->getData();
+    send_config = false;
+  }
+  
+}
+
---
+++ regtable.ino
@@ -0,0 +1,276 @@
+/**
+ * regtable
+ *
+ * Copyright (c) 2015 Daniel Berenguer <dberenguer@usapiens.com>
+ * 
+ * This file is part of the panStamp project.
+ * 
+ * panStamp  is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * any later version.
+ * 
+ * panStamp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with panStamp; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 
+ * USA
+ * 
+ * Author: Daniel Berenguer
+ * Creation date: 04/01/2015
+ */
+
+#include "product.h"
+#include "regtable.h"
+
+STORAGE nvMem;
+uint8_t MemoryValue[2];
+
+/**
+ * Definition of common registers
+ */
+DEFINE_COMMON_REGISTERS()
+
+/*
+ * Definition of custom registers
+ */
+// setup register
+byte dtConfiguration[14];
+/*
+ * dtConfiguration[0] => Pin 10 gehört zur Gruppe <GroupNumber>
+ * dtConfiguration[1] => Pin 11 gehört zur Gruppe <GroupNumber>
+ * dtConfiguration[2] => Pin 12 gehört zur Gruppe <GroupNumber>
+ * dtConfiguration[3] => Pin 13 gehört zur Gruppe <GroupNumber>
+ * dtConfiguration[4] => Pin 14 gehört zur Gruppe <GroupNumber>
+ * dtConfiguration[5] => Pin 15 gehört zur Gruppe <GroupNumber>
+ * dtConfiguration[6] => Pin 16 gehört zur Gruppe <GroupNumber>
+ * dtConfiguration[7] => Pin 17 gehört zur Gruppe <GroupNumber>
+ * dtConfiguration[8,9] => BlinkOnEvtTime
+ * dtConfiguration[10,11] => BlinkOnIdleTime
+ * dtConfiguration[12,13] => BlinkOffTime
+ **/
+REGISTER regConfiguration(dtConfiguration,
+                            sizeof(dtConfiguration),
+                            &GetConfiguration,
+                            &SetConfiguration);
+// Binary input register
+REGISTER regBinInputs(dtBinInputs, sizeof(dtBinInputs), NULL, NULL);
+
+// GroupStates 
+REGISTER regGroupStates(group_state, sizeof(group_state), &UpdateGroupState, NULL);
+
+/**
+ * Initialize table of registers
+ */
+DECLARE_REGISTERS_START()
+  &regConfiguration,                  // Register ID = 11 (0x0B)
+  &regBinInputs,                      // Register ID = 12 (0x0C)
+  &regGroupStates                     // Register ID = 13 (0x0D)
+DECLARE_REGISTERS_END()
+
+/**
+ * Definition of common getter/setter callback functions
+ */
+DEFINE_COMMON_CALLBACKS()
+
+/**
+ * Definition of custom getter/setter callback functions
+ */
+ 
+/**
+ * setConfiguration
+ *
+ * Set Confuguration and store in EEPROM
+ *
+ * 'rId'         Register ID
+ * 'value'       New value
+ */
+const void SetConfiguration(byte rId, byte *value) {
+  // Update register
+  debug("Configuration received", true);
+  if ((value[0] != regTable[rId]->value[0]) ||            \
+      (value[1] != regTable[rId]->value[1]) ||            \
+      (value[2] != regTable[rId]->value[2]) ||            \
+      (value[3] != regTable[rId]->value[3]) ||            \
+      (value[4] != regTable[rId]->value[4]) ||            \
+      (value[5] != regTable[rId]->value[5]) ||            \
+      (value[6] != regTable[rId]->value[6]) ||            \
+      (value[7] != regTable[rId]->value[7]) ||            \
+      (value[8] != regTable[rId]->value[8]) ||            \
+      (value[9] != regTable[rId]->value[9]) ||            \
+      (value[10] != regTable[rId]->value[10]) ||            \
+      (value[11] != regTable[rId]->value[11]) ||            \
+      (value[12] != regTable[rId]->value[12]) ||            \
+      (value[13] != regTable[rId]->value[13]))              \
+  {
+    debug("Configuration writing ....", true);
+ 
+    // write into info/eeprom memory
+    for(unsigned int i=0;i<=13;i++) { 
+      MemoryValue[0] = value[i];
+      // INFOMEM_SECTION_B: pos 0 bis INFOMEM_SECTION_B: pos 7
+      nvMem.write(&MemoryValue[0], INFOMEM_SECTION_B, i, sizeof(MemoryValue[0]));
+
+      debug("Loop ", false);
+      debug_dec(i, false);
+      debug(": ", false);
+      debug_hex(MemoryValue[0], false);
+      debug(" written", false);
+
+      nvMem.read(&MemoryValue[1], INFOMEM_SECTION_B, i, sizeof(MemoryValue[1]));
+      debug("  -->  ", false);
+      debug("read: ", false);
+      debug_hex(MemoryValue[1], true);
+    }
+    // neue Parameter in die Variablen laden
+    getVarsFromEeprom();
+
+    // Transmit new Configuration 
+    send_config = true;
+  }
+}
+
+/**
+ * GetConfiguration
+ *
+ * Collect ConfigurationParameters in Register
+ *
+ * 'rId'         Register ID
+ * 'value'       New value
+ */
+const void GetConfiguration(byte rId) {
+  // Update register
+  for(unsigned int i=0;i<=7;i++) { 
+    dtConfiguration[i] = group_assignment[i];
+  }
+
+  dtConfiguration[8]  = (LED_BlinkOnEvtTime >> 0x08) & 0xFF;
+  dtConfiguration[9]  =  LED_BlinkOnEvtTime & 0xFF;
+
+  dtConfiguration[10] = (LED_BlinkOnIdleTime >> 0x08) & 0xFF;
+  dtConfiguration[11] =  LED_BlinkOnIdleTime & 0xFF;
+
+  dtConfiguration[12] = (LED_BlinkOffTime >> 0x08) & 0xFF;
+  dtConfiguration[13] =  LED_BlinkOffTime & 0xFF;
+} 
+
+
+void getVarsFromEeprom() {
+  uint8_t myVal = 0;
+
+  //initialization
+  for(unsigned int i=0;i<8;i++) {
+    group_count[i]      = 0;
+    group_assignment[i] = 0;
+  }
+     
+  // getting group definitions
+  // INFOMEM_SECTION_B: pos 0 bis INFOMEM_SECTION_B: pos 7
+  for(unsigned int i=0;i<=7;i++) { 
+    nvMem.read(&MemoryValue[0], INFOMEM_SECTION_B, 13-i, sizeof(MemoryValue[0]));
+    myVal = (int)MemoryValue[0] & 0x07; // Gruppennummer, nur die ersten 3 Bit -> nur Range 0-7 ist valid
+    //myVal = MemoryValue[0];
+    group_assignment[i] = myVal;
+    group_count[myVal]++;
+
+    debug("Pin 1", false);
+    debug_hex(i, false);
+    debug("belongs to Group ", false);
+    debug_hex(myVal, true);
+  } 
+
+  nvMem.read(&MemoryValue[0], INFOMEM_SECTION_B, 4, sizeof(MemoryValue[0]));
+  nvMem.read(&MemoryValue[1], INFOMEM_SECTION_B, 5, sizeof(MemoryValue[1]));
+  LED_BlinkOnEvtTime = (MemoryValue[0] << 8) | MemoryValue[1];
+  
+  debug("LED_BlinkOnEvtTime = ", false);
+  debug_hex(LED_BlinkOnEvtTime, true);
+  
+  nvMem.read(&MemoryValue[0], INFOMEM_SECTION_B, 2, sizeof(MemoryValue[0]));
+  nvMem.read(&MemoryValue[1], INFOMEM_SECTION_B, 3, sizeof(MemoryValue[1]));
+  LED_BlinkOnIdleTime = (MemoryValue[0] << 8) | MemoryValue[1];
+  debug("LED_BlinkOnIdleTime = ", false);
+  debug_hex(LED_BlinkOnIdleTime, true);
+  
+  nvMem.read(&MemoryValue[0], INFOMEM_SECTION_B, 0, sizeof(MemoryValue[0]));
+  nvMem.read(&MemoryValue[1], INFOMEM_SECTION_B, 1, sizeof(MemoryValue[1]));
+  LED_BlinkOffTime = (MemoryValue[0] << 8) | MemoryValue[1];
+  debug("LED_BlinkOffTime = ", false);
+  debug_hex(LED_BlinkOffTime, true);
+}
+
+const void UpdateGroupState(byte rID) {
+  // Update GroupStates for TX
+  uint8_t group_sum[8]; // group_sum[GroupNumber] = Summe der Stati
+  
+  //initialization
+  for(unsigned int i=0;i<8;i++) {
+    group_state[i] = 0;
+    group_sum[i] = 0;
+  }
+  
+  // ueber alle Pins
+  group_sum[group_assignment[0]] += (dtBinInputs[0] & 0x01);
+  group_sum[group_assignment[1]] += (dtBinInputs[0] & 0x02) >> 0x01 ;
+  group_sum[group_assignment[2]] += (dtBinInputs[0] & 0x04) >> 0x02 ;
+  group_sum[group_assignment[3]] += (dtBinInputs[0] & 0x08) >> 0x03 ;
+  group_sum[group_assignment[4]] += (dtBinInputs[0] & 0x10) >> 0x04 ;
+  group_sum[group_assignment[5]] += (dtBinInputs[0] & 0x20) >> 0x05 ;
+  group_sum[group_assignment[6]] += (dtBinInputs[0] & 0x40) >> 0x06 ;
+  group_sum[group_assignment[7]] += (dtBinInputs[0] & 0x80) >> 0x07 ;
+
+  for(unsigned int i=0;i<=7;i++) {
+    if(group_sum[i] == 0) group_state[i] = 0; // closed
+    else if(group_sum[i] == group_count[i]) group_state[i] = 2; // opened
+    else group_state[i] = 1; // tilted
+  }
+
+  debug("Binary State: ", false);
+  debug_bin(dtBinInputs[0],true);
+
+  debug("Status Group Assignment: ", false);
+  debug_hex(group_assignment[7],false);
+  debug_hex(group_assignment[6],false);
+  debug_hex(group_assignment[5],false);
+  debug_hex(group_assignment[4],false);
+  debug_hex(group_assignment[3],false);
+  debug_hex(group_assignment[2],false);
+  debug_hex(group_assignment[1],false);
+  debug_hex(group_assignment[0],true);
+  
+  debug("Status Group_Count: ", false);
+  debug_hex(group_count[7],false);
+  debug_hex(group_count[6],false);
+  debug_hex(group_count[5],false);
+  debug_hex(group_count[4],false);
+  debug_hex(group_count[3],false);
+  debug_hex(group_count[2],false);
+  debug_hex(group_count[1],false);
+  debug_hex(group_count[0],true);
+  
+  debug("Status Group_Sum: ", false);
+  debug_hex(group_sum[7],false);
+  debug_hex(group_sum[6],false);
+  debug_hex(group_sum[5],false);
+  debug_hex(group_sum[4],false);
+  debug_hex(group_sum[3],false);
+  debug_hex(group_sum[2],false);
+  debug_hex(group_sum[1],false);
+  debug_hex(group_sum[0],true);
+  
+  debug("Status Group_State: ", false);
+  debug_hex(group_state[7],false);
+  debug_hex(group_state[6],false);
+  debug_hex(group_state[5],false);
+  debug_hex(group_state[4],false);
+  debug_hex(group_state[3],false);
+  debug_hex(group_state[2],false);
+  debug_hex(group_state[1],false);
+  debug_hex(group_state[0],true);
+  
+}
+
